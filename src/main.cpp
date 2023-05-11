07d028f94a75dd5da93c2988933cfa1dbd51486d
qtcv <liuwen.yzu@gmail.com>
2023-05-11 10:31:43 +0800

    release

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,31 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    if (argc == 2)
+    {
+        const QString version = APP_VERSION;
+
+        if ((!strncmp(argv[1], "--version", 9)))
+        {
+            qInfo() << QString("pose ") << version << "\n";
+            std::cout << "pose " << version.toStdString() << "\n";
+            return 0;
+        }
+    }
+
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
+    for (const QString &locale : uiLanguages)
+    {
         const QString baseName = "pose2_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
+        if (translator.load(":/i18n/" + baseName))
+        {
             a.installTranslator(&translator);
             break;
         }
