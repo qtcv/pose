784e0d5e9d7b6e74f777ea678b3e6b8334d64371
qtcv <liuwen.yzu@gmail.com>
2023-05-24 08:41:17 +0800

    ui

--- src/pose.h
+++ src/pose.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QTimer>
+#include <QFileDialog>
 #include "login_dialog.h"
 #include "points_dialog.h"
 #include "choose_task_dialog.h"
@@ -29,7 +30,21 @@ private slots:
 
     void on_choose_task_triggered();
 
+    void on_open_act_triggered();
+
+    void on_save_act_triggered();
+
+    void on_play_btn_clicked();
+
 private:
     Ui::pose *ui;
+
+    QString _config_path = QString("config.ini");
+    QString _user_name;
+    QString _passwd;
+    QString _source_path = QString("/Users/liuwen/Downloads/2023.mp4");;
+    QString _default_open_dir;
+    QString _default_save_dir;
+
 };
 #endif // POSE_H
